a0ea8ef865c7704af18584fcbbeac38aaa6b4d89
Henry Van Styn <vanstyn@intellitree.com>
2015-06-02 20:36:57 -0400

    DemoAuthOvr plugin now manually sets the authed user
    No longer needs to supply the pass, so it will still work if the
    pass is changed

--- lib/YN2015/Plugin/DemoAuthOvr.pm
+++ lib/YN2015/Plugin/DemoAuthOvr.pm
@@ -5,17 +5,22 @@ use namespace::autoclean;
 use RapidApp::Util qw(:all);
 
 # This is a special plugin for demo use only -- will auto-login
-# as admin/pass, if needed, whenever the request referer contains 
+# as the user 'admin', if needed, whenever the request referer contains 
 # /slides/ which is generated from the iframes in the slideshow
 
 before 'dispatch' => sub {
   my $c = shift;
   
+  my $username = 'admin';
+  
   my $referer = $c->req->header('referer') || '';
   if($referer =~ /slides/) {
     if(my $AuthC = $c->controller('Auth')) {
       unless ($c->session && $c->session_is_valid and $c->user_exists) {
-        $AuthC->do_login($c,'admin','pass');
+        my $user = $c->find_user({ username => $username }) or die "Failed to find user '$username'";
+        $c->set_authenticated( $user );
+        $c->session->{RapidApp_username} = $username;
+        $c->res->header('X-RapidApp-Authenticated' => $username);
       }
     }
   }
